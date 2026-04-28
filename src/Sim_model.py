04834bfbc176a59e16f484b1a8b4bb61a65fbd3e
ETIENNEGRIFFON <112128327+ETIENNEGRIFFON@users.noreply.github.com>
2026-04-28 13:56:12 +0300

    Update Sim_model.py

--- src/Sim_model.py
+++ src/Sim_model.py
@@ -1,1 +1,9 @@
 # SIM model code will go here
+import pandas as pd
+
+file_path = "data/Sim Model Portfolio For Code.ods"
+
+excel_file = pd.ExcelFile(file_path, engine="odf")
+
+print("Sheets in this file:")
+print(excel_file.sheet_names)
